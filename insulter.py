4a813b2be3af6a1c24e463194fd36727884b366e
OgJAkfy8 <happold77-github@yahoo.com>
2021-07-19 08:40:29 -0500

    Cleanup

--- insulter.py
+++ insulter.py
@@ -1,11 +1,17 @@
+# Import libraries
 from random import randint
 
+# Assign variables
+name = insult  = "."
+count = 0
 
-insults = ["Pig Head", "Booby Head", "Baby Baby","poopy head","fart face","booby burper","gonk face","chicken"]
+# Initate insult array
+insults = ["Pig Head", "Big Head", "Baby Baby","poopy head","fart face","booby burper","gonk face","chicken"]
 
+#Functions (Random Insults and Greeting generator)
 def randInsult():
-    insultz = insults[(randint(1,len(insults)))]
-    return insultz;
+    num = randint(0,(len(insults)-1))
+    return(insults[num]);
 
 def Greeting(name):
     if (name == "Alex"):
@@ -14,16 +20,36 @@ def Greeting(name):
         print("Thanks for playing.")
     elif ((name == "mom") or (name == "dad" )):
         print(name+ " you look fantastic today.")
+    elif (name == 'quit'):
+        print('So. I see you are a quitter.  \nYou loose 50 points')
     else:
         print("Hello " +name+ " you are a " +insult)
     return;
 
+def dots(num):
+    print('.' * num);
 
-print(".............Welcome to the insult machine.........")
-name = "."
-insult = randInsult()
-while name != "stop":
-    print('\n' * 13)
-    name = input("What is your Name? ")
+def dashs(num):
+    print('-' * num);
+
+def lines(num):
+    print("\n" * num)
+
+# Program header
+lines(4)
+dots(25)
+print('Welcome to the insult machine')
+lines(1)
+    
+# Program
+while name != "quit":
+    if(count == 5):
+        print('Enter "quit" to quit')
+        count = 0
+
+    name = input('What is your Name? ')
     insult = randInsult()
     Greeting (name)
+    dashs(25)
+    lines(1)
+    count += 1
